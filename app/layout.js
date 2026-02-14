5b69bd6bab538ac3dfb65c133ae61d118650be6c
Hussain <hussaintmerng@gmail.com>
2026-02-14 19:56:00 +0500

    upload meta

--- app/layout.js
+++ app/layout.js
@@ -26,8 +26,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <meta name="google-site-verification" content="googledcb838c8b9d12403.html" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="google-site-verification" content="googledcb838c8b9d12403.html" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
